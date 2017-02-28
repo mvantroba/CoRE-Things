c8b171845fa96b561dd2173ba50f99cae656b138
martinv <martinvantroba@hotmail.com>
2017-02-28 17:14:33 +0100

    Add Javadoc

--- rd/src/main/java/de/thk/rdw/rd/uri/UriVariableValidator.java
+++ rd/src/main/java/de/thk/rdw/rd/uri/UriVariableValidator.java
@@ -1,5 +1,11 @@
 package de.thk.rdw.rd.uri;
 
+/**
+ * Resource directory entries can be exported to other discovery mechanisms such
+ * as DNS-SD. For that reason, parameters should be limited to a maximum length
+ * of 63 Bytes.
+ *
+ */
 public interface UriVariableValidator {
 
 	void validate(String value);

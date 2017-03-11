37093ade890a7bb0d4518ada2423e70ad3f8a6b3
martinv <martinvantroba@hotmail.com>
2017-03-11 18:24:05 +0100

    Build entire resource tree

--- admin/src/main/java/de/thk/rdw/admin/controller/TreeUtils.java
+++ admin/src/main/java/de/thk/rdw/admin/controller/TreeUtils.java
@@ -1,5 +1,10 @@
 package de.thk.rdw.admin.controller;
 
+import java.util.Scanner;
+import java.util.Set;
+
+import org.eclipse.californium.core.WebLink;
+import org.eclipse.californium.core.coap.LinkFormat;
 import org.eclipse.californium.core.coap.Response;
 
 import de.thk.rdw.admin.model.TreeItemResource;
@@ -18,6 +23,42 @@ public class TreeUtils {
 		} else {
 			result = new TreeItem<>(home);
 		}
+		result.setExpanded(true);
+		addResources(result, response.getPayloadString(), onlyRd, showIcons);
 		return result;
 	}
+
+	private static void addResources(TreeItem<TreeItemResource> rootItem, String payloadString, boolean onlyRd,
+			boolean showIcons) {
+		Set<WebLink> links = LinkFormat.parse(payloadString);
+		TreeItem<TreeItemResource> parent = rootItem;
+		for (WebLink link : links) {
+			Scanner scanner = new Scanner(link.getURI());
+			scanner.useDelimiter("/");
+			while (scanner.hasNext()) {
+				String resourceName = scanner.next();
+				TreeItem<TreeItemResource> resource = findChildResource(parent, resourceName);
+				if (resource == null) {
+					resource = new TreeItem<>(new TreeItemResource(resourceName));
+					resource.setExpanded(true);
+					parent.getChildren().add(resource);
+				}
+				parent = resource;
+			}
+			parent = rootItem;
+			scanner.close();
+		}
+	}
+
+	private static TreeItem<TreeItemResource> findChildResource(TreeItem<TreeItemResource> parent,
+			String resourceName) {
+		TreeItem<TreeItemResource> result = null;
+		for (TreeItem<TreeItemResource> child : parent.getChildren()) {
+			if (resourceName.equals(child.getValue().getName())) {
+				result = child;
+			}
+		}
+		return result;
+	}
+
 }

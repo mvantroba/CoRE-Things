24fa0a83c76b5d88b9f62afd46274f4d5b55bf9b
martinv <martinvantroba@hotmail.com>
2017-03-12 19:48:45 +0100

    Replace menu with tab pane in root layout

--- admin/src/main/java/de/thk/rdw/admin/AdminApplication.java
+++ admin/src/main/java/de/thk/rdw/admin/AdminApplication.java
@@ -9,56 +9,57 @@ import de.thk.rdw.admin.controller.ManageController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AdminApplication extends Application {
 
-	private static final Logger LOGGER = Logger.getLogger(AdminApplication.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(AdminApplication.class.getName());
 
-	private static final String BUNDLE_BASE_NAME = "i18n/bundle";
+    private static final String BUNDLE_BASE_NAME = "i18n/bundle";
 
-	private Stage primaryStage;
-	private BorderPane rootLayout;
+    private Stage primaryStage;
+    private VBox rootLayout;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Resource Directory Admin");
-		initRootLayout();
-		showManage();
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Resource Directory Admin");
+        initRootLayout();
+        showManage();
+    }
 
-	private void initRootLayout() {
-		try {
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(AdminApplication.class.getResource("/fxml/Root.fxml"));
-			loader.setResources(ResourceBundle.getBundle(BUNDLE_BASE_NAME));
-			rootLayout = loader.load();
-			Scene scene = new Scene(rootLayout);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
-		}
-	}
+    private void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(AdminApplication.class.getResource("/fxml/Root.fxml"));
+            loader.setResources(ResourceBundle.getBundle(BUNDLE_BASE_NAME));
+            rootLayout = loader.load();
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
 
-	private void showManage() {
-		try {
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(AdminApplication.class.getResource("/fxml/Manage.fxml"));
-			loader.setResources(ResourceBundle.getBundle(BUNDLE_BASE_NAME));
-			VBox manageLayout = loader.load();
-			ManageController controller = loader.getController();
-			controller.setPrimaryStage(primaryStage);
-			rootLayout.setCenter(manageLayout);
-		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
-		}
-	}
+    private void showManage() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(AdminApplication.class.getResource("/fxml/Manage.fxml"));
+            loader.setResources(ResourceBundle.getBundle(BUNDLE_BASE_NAME));
+            VBox manageLayout = loader.load();
+            ManageController controller = loader.getController();
+            controller.setPrimaryStage(primaryStage);
+            TabPane node = (TabPane) rootLayout.getChildren().get(0);
+            node.getTabs().get(0).setContent(manageLayout);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
 }

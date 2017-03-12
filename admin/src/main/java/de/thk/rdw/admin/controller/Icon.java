9c0dab0e23e16d7c222f36cd261e1219780e0c26
martinv <martinvantroba@hotmail.com>
2017-03-12 16:30:34 +0100

    Lazy loading of icons, add additional icons

--- admin/src/main/java/de/thk/rdw/admin/controller/Icon.java
+++ admin/src/main/java/de/thk/rdw/admin/controller/Icon.java
@@ -7,24 +7,30 @@ import javafx.scene.image.ImageView;
 
 public enum Icon {
 
-	HOME_GREEN_24("ic_home_green_24x24.png"), INFO_BLUE_36("ic_info_blue_36x36.png"), CHECK_CIRCLE_GREEN_36(
-			"ic_check_circle_green_36x36.png"), ERROR_RED_36("ic_error_red_36x36.png");
+	HOME_GREEN_24("ic_home_green_24x24.png"), PUBLIC_BLUE_24("ic_public_blue_24x24.png"), FOLDER_AMBER_24(
+			"ic_folder_amber_24x24.png"), GROUP_WORK_GREY_24("ic_group_work_grey_24x24.png"), PAGEVIEW_GREY_24(
+					"ic_pageview_grey_24x24.png"), RASPBERRY_24("ic_raspberry_24x24.png"), OPENHAB_24(
+							"ic_openhab_24x24.png"), ENDPOINT_GREY_24("ic_endpoint_grey_24x24.png"), INFO_BLUE_36(
+									"ic_info_blue_36x36.png"), CHECK_CIRCLE_GREEN_36(
+											"ic_check_circle_green_36x36.png"), ERROR_RED_36("ic_error_red_36x36.png");
 
 	private static final String DIRECTORY = "img";
 
 	private String fileName;
+	private ImageView imageView;
 
 	private Icon(String fileName) {
 		this.fileName = fileName;
 	}
 
 	public ImageView getImageView() {
-		ImageView result = null;
-		InputStream inputStream = getClass().getResourceAsStream(getPath());
-		if (inputStream != null) {
-			result = new ImageView(new Image(inputStream));
+		if (imageView == null) {
+			InputStream inputStream = getClass().getResourceAsStream(getPath());
+			if (inputStream != null) {
+				imageView = new ImageView(new Image(inputStream));
+			}
 		}
-		return result;
+		return imageView;
 	}
 
 	private String getPath() {

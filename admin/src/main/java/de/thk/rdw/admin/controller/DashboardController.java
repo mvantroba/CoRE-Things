754be7c76fd2be5e9a74cb8a463a4b79b450b015
martinv <martinvantroba@hotmail.com>
2017-03-14 21:57:40 +0100

    Inject notification controller into dashboard controller

--- admin/src/main/java/de/thk/rdw/admin/controller/DashboardController.java
+++ admin/src/main/java/de/thk/rdw/admin/controller/DashboardController.java
@@ -1,6 +1,5 @@
 package de.thk.rdw.admin.controller;
 
-import java.io.IOException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,17 +9,13 @@ import org.eclipse.californium.core.coap.MessageObserverAdapter;
 import org.eclipse.californium.core.coap.Request;
 import org.eclipse.californium.core.coap.Response;
 
-import de.thk.rdw.admin.AdminApplication;
 import de.thk.rdw.admin.model.TreeItemResource;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeView;
-import javafx.scene.layout.HBox;
 
 public class DashboardController {
 
@@ -38,30 +33,13 @@ public class DashboardController {
 	private TextField variables;
 	@FXML
 	private TreeView<TreeItemResource> resourceTree;
-
 	@FXML
-	private HBox notificationContainer;
-
-	private NotificationController notification;
+	private NotificationController notificationController;
 
 	@FXML
 	private void initialize() {
 		scheme.setItems(FXCollections.observableArrayList(CoAP.COAP_URI_SCHEME, CoAP.COAP_SECURE_URI_SCHEME));
 		scheme.getSelectionModel().select(0);
-
-		String path = String.format("%s/%s", AdminApplication.FXML_BASE_FOLDER, "Notification.fxml");
-		Node notificationLayout;
-		FXMLLoader loader;
-		try {
-			loader = new FXMLLoader();
-			loader.setLocation(MainController.class.getResource(path));
-			loader.setResources(resources);
-			notificationLayout = loader.load();
-			notification = loader.getController();
-			notificationContainer.getChildren().add(notificationLayout);
-		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
-		}
 	}
 
 	@FXML
@@ -78,7 +56,7 @@ public class DashboardController {
 
 					@Override
 					public void run() {
-						notification.success("notification.discovery.success");
+						notificationController.success("notification.discovery.success");
 						populateTree(response);
 					}
 				});
@@ -86,7 +64,7 @@ public class DashboardController {
 		});
 		LOGGER.log(Level.INFO, "Sending GET request to \"{0}\"...", uri);
 		request.send();
-		notification.spinner(resources.getString("notification.discovery.requestSent"));
+		notificationController.spinner(resources.getString("notification.discovery.requestSent"));
 	}
 
 	@FXML

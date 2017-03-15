2c175384a951b9d7ecb071a3e2c88cc1fe22f341
martinv <martinvantroba@hotmail.com>
2017-03-15 19:13:47 +0100

    Remove public constants

--- admin/src/main/java/de/thk/rdw/admin/AdminApplication.java
+++ admin/src/main/java/de/thk/rdw/admin/AdminApplication.java
@@ -15,16 +15,13 @@ public class AdminApplication extends Application {
 
 	private static final Logger LOGGER = Logger.getLogger(AdminApplication.class.getName());
 
-	public static final String BUNDLE_BASE_NAME = "i18n/bundle";
-	public static final String FXML_BASE_FOLDER = "/fxml";
-
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_BASE_NAME);
+		ResourceBundle bundle = ResourceBundle.getBundle("i18n/bundle");
 		Scene scene = new Scene(loadMainLayout(bundle));
 		primaryStage.setTitle(bundle.getString("project.name"));
 		primaryStage.setScene(scene);
@@ -33,11 +30,10 @@ public class AdminApplication extends Application {
 
 	private Parent loadMainLayout(ResourceBundle bundle) {
 		Parent result = null;
-		String path = String.format("%s/%s", FXML_BASE_FOLDER, "Main.fxml");
 		FXMLLoader loader;
 		try {
 			loader = new FXMLLoader();
-			loader.setLocation(AdminApplication.class.getResource(path));
+			loader.setLocation(AdminApplication.class.getResource("/fxml/Main.fxml"));
 			loader.setResources(bundle);
 			result = loader.load();
 		} catch (IOException e) {

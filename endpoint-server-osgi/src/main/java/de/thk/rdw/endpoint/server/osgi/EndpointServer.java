2794428dabc924040a233b130fe82089d8c22a2b
martinv <martinvantroba@hotmail.com>
2017-03-16 13:49:32 +0100

    Bind endpoints to all network interfaces after start

--- endpoint-server-osgi/src/main/java/de/thk/rdw/endpoint/server/osgi/EndpointServer.java
+++ endpoint-server-osgi/src/main/java/de/thk/rdw/endpoint/server/osgi/EndpointServer.java
@@ -1,12 +1,24 @@
 package de.thk.rdw.endpoint.server.osgi;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.core.network.EndpointManager;
 
 public class EndpointServer extends CoapServer {
 
 	public EndpointServer() {
 		super();
+		// Bind endpoints to each network interface.
+		for (InetAddress address : EndpointManager.getEndpointManager().getNetworkInterfaces()) {
+			if (!address.isLinkLocalAddress()) {
+				this.addEndpoint(new CoapEndpoint(new InetSocketAddress(address, CoAP.DEFAULT_COAP_PORT)));
+			}
+		}
 		add(new CoapResource("light"));
 	}
 }

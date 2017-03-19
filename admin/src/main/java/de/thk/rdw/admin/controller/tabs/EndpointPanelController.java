d03372db03388b3d006303e08f3a21594dfff4d0
martinv <martinvantroba@hotmail.com>
2017-03-19 13:46:09 +0100

    Enable user to add and remove endpoint panels on dashboard

--- admin/src/main/java/de/thk/rdw/admin/controller/tabs/EndpointPanelController.java
+++ admin/src/main/java/de/thk/rdw/admin/controller/tabs/EndpointPanelController.java
@@ -7,8 +7,11 @@ import de.thk.rdw.admin.icon.EndpointTypeIcon;
 import de.thk.rdw.admin.icon.Icon;
 import de.thk.rdw.admin.icon.IconSize;
 import de.thk.rdw.admin.model.GuiCoapResource;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
@@ -26,10 +29,16 @@ public class EndpointPanelController implements Initializable {
 	private Label lifetime;
 	@FXML
 	private Label context;
+	@FXML
+	private Button close;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+	}
+
+	public void setOnCloseEventHandler(EventHandler<ActionEvent> eventHandler) {
+		close.setOnAction(eventHandler);
 	}
 
 	public void setGuiCoapResource(GuiCoapResource guiCoapResource) {

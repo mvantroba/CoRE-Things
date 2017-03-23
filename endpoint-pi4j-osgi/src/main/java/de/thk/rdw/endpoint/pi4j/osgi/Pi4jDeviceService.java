47f2e099945ee35a12042c7a9abc001dba95a044
martinv <martinvantroba@hotmail.com>
2017-03-23 16:00:11 +0100

    Add state change listener to tilt sensor

--- endpoint-pi4j-osgi/src/main/java/de/thk/rdw/endpoint/pi4j/osgi/Pi4jDeviceService.java
+++ endpoint-pi4j-osgi/src/main/java/de/thk/rdw/endpoint/pi4j/osgi/Pi4jDeviceService.java
@@ -10,6 +10,8 @@ import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 import de.thk.rdw.endpoint.device.osgi.DeviceService;
 
@@ -27,6 +29,13 @@ public class Pi4jDeviceService implements DeviceService {
 		led.setShutdownOptions(true, PinState.LOW);
 		tilt = gpioController.provisionDigitalInputPin(RaspiPin.GPIO_28, "tilt");
 		tilt.setShutdownOptions(true);
+		tilt.addListener(new GpioPinListenerDigital() {
+
+			@Override
+			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+				LOGGER.log(Level.INFO, "Tilt state changed: \"{0}\"", event.getState().getName());
+			}
+		});
 	}
 
 	@Override
@@ -35,16 +44,18 @@ public class Pi4jDeviceService implements DeviceService {
 		if (pin != null) {
 			((GpioPinDigitalOutput) pin).toggle();
 			LOGGER.log(Level.INFO, "Toggled actuator with name \"{0}\".", new Object[] { name });
-			LOGGER.log(Level.INFO, "Tilt state: \"{0}\"", new Object[] { tilt.getState().getName() });
 		} else {
 			LOGGER.log(Level.WARNING, "Actuator with name \"{0}\" does not exists.", new Object[] { name });
 		}
-
 	}
 
 	public void deactivate() {
 		led.setState(PinState.LOW);
-		gpioController.shutdown();
+
+		// shutdown() causes java.util.concurrent.RejectedExecutionException in
+		// listeners after bundle is started again.
+		// gpioController.shutdown();
+
 		gpioController.unprovisionPin(led);
 		gpioController.unprovisionPin(tilt);
 		LOGGER.log(Level.INFO, "GPIO controller has been shut down.");

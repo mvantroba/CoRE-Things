6e0bbc52cbbc372aa6ee3d0cccf437e70a3c6935
martinv <martinvantroba@hotmail.com>
2017-03-26 16:37:10 +0200

    Add method for pinging server

--- admin/src/main/java/de/thk/rdw/admin/coap/ExtendedCoapClient.java
+++ admin/src/main/java/de/thk/rdw/admin/coap/ExtendedCoapClient.java
@@ -1,6 +1,7 @@
 package de.thk.rdw.admin.coap;
 
 import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.coap.CoAP.Type;
 import org.eclipse.californium.core.coap.MessageObserver;
 import org.eclipse.californium.core.coap.Request;
 
@@ -13,6 +14,13 @@ public class ExtendedCoapClient extends CoapClient {
 	public void discover(MessageObserver observer) {
 		Request request = Request.newGet().setURI(getURI());
 		request.getOptions().clearUriPath().clearUriQuery().setUriPath("/.well-known/core");
+		request.addMessageObserver(observer);
+		request.send();
+	}
+
+	public void ping(MessageObserver observer) {
+		Request request = new Request(null, Type.CON).setURI(getURI());
+		request.setToken(new byte[0]);
 		request.addMessageObserver(observer);
 		request.send();
 	}

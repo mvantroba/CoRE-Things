6ff4b68a641aa1adc385c3e5475fefb821c80ffe
martinv <martinvantroba@hotmail.com>
2017-05-01 00:51:06 +0200

    Create device resource factory, minor refractoring

--- endpoint-pi4j-osgi/src/main/java/de/thk/rdw/endpoint/pi4j/osgi/Pi4jDeviceService.java
+++ endpoint-pi4j-osgi/src/main/java/de/thk/rdw/endpoint/pi4j/osgi/Pi4jDeviceService.java
@@ -12,13 +12,9 @@ import java.util.logging.Logger;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.Pin;
-import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListener;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 import de.thk.rdw.base.ActuatorType;
+import de.thk.rdw.base.ResourceType;
 import de.thk.rdw.base.SensorType;
 import de.thk.rdw.endpoint.device.osgi.DeviceListener;
 import de.thk.rdw.endpoint.device.osgi.DeviceService;
@@ -26,33 +22,29 @@ import de.thk.rdw.endpoint.device.osgi.NoSuchActuatorException;
 import de.thk.rdw.endpoint.device.osgi.NoSuchSensorException;
 import de.thk.rdw.endpoint.pi4j.osgi.data.CsvReader;
 import de.thk.rdw.endpoint.pi4j.osgi.resources.ActuatorResource;
+import de.thk.rdw.endpoint.pi4j.osgi.resources.DeviceResourceFactory;
+import de.thk.rdw.endpoint.pi4j.osgi.resources.DeviceResourceListener;
 import de.thk.rdw.endpoint.pi4j.osgi.resources.SensorResource;
-import de.thk.rdw.endpoint.pi4j.osgi.resources.gpio.LedResource;
-import de.thk.rdw.endpoint.pi4j.osgi.resources.gpio.MercurySwitchResource;
-import de.thk.rdw.endpoint.pi4j.osgi.resources.gpio.PirSensorResource;
-import de.thk.rdw.endpoint.pi4j.osgi.resources.gpio.TactileSwitchResource;
 
 public class Pi4jDeviceService implements DeviceService {
 
 	private static final Logger LOGGER = Logger.getLogger(Pi4jDeviceService.class.getName());
 
-	private List<DeviceListener> deviceListeners = new ArrayList<>();
+	private GpioController gpioController;
 
 	private NavigableMap<Integer, SensorResource> sensors = new TreeMap<>();
 	private NavigableMap<Integer, ActuatorResource> actuators = new TreeMap<>();
 
-	private GpioController gpioController;
+	private List<DeviceListener> deviceListeners = new ArrayList<>();
 
 	public Pi4jDeviceService() {
 		gpioController = GpioFactory.getInstance();
-
-		CsvReader.read();
-
-		addActuator("led", ActuatorType.LED, RaspiPin.GPIO_00);
-
-		addSensor("tilt", SensorType.MERCURY_SWITCH, RaspiPin.GPIO_01);
-		addSensor("button", SensorType.TACTILE_SWITCH, RaspiPin.GPIO_02);
-		addSensor("motion", SensorType.PIR_SENSOR, RaspiPin.GPIO_03);
+		for (String[] sensorArguments : CsvReader.readResources(ResourceType.SENSOR)) {
+			addSensor(sensorArguments);
+		}
+		for (String[] actuatorArguments : CsvReader.readResources(ResourceType.ACTUATOR)) {
+			addActuator(actuatorArguments);
+		}
 	}
 
 	@Override
@@ -70,10 +62,9 @@ public class Pi4jDeviceService implements DeviceService {
 		ActuatorResource actuator = actuators.get(id);
 		if (actuator != null) {
 			actuator.toggle();
-			LOGGER.log(Level.INFO, "Toggled \"{0}\".", new Object[] { actuator.toString() });
+			LOGGER.log(Level.INFO, "Toggled {0}.", new Object[] { actuator.toString() });
 		} else {
-			throw new NoSuchActuatorException(
-					String.format("Actuator with ID \"%d\" does not exists on the device.", id));
+			throw new NoSuchActuatorException(String.format("Actuator with ID %d does not exist on the device.", id));
 		}
 	}
 
@@ -104,7 +95,7 @@ public class Pi4jDeviceService implements DeviceService {
 		if (sensor != null) {
 			result = sensor.getValue();
 		} else {
-			throw new NoSuchSensorException(String.format("Sensor with ID %d does not exists on the device.", id));
+			throw new NoSuchSensorException(String.format("Sensor with ID %d does not exist on the device.", id));
 		}
 		return result;
 	}
@@ -116,7 +107,7 @@ public class Pi4jDeviceService implements DeviceService {
 		if (actuator != null) {
 			result = actuator.getValue();
 		} else {
-			throw new NoSuchActuatorException(String.format("Actuator with ID %d does not exists on the device.", id));
+			throw new NoSuchActuatorException(String.format("Actuator with ID %d does not exist on the device.", id));
 		}
 		return result;
 	}
@@ -125,22 +116,20 @@ public class Pi4jDeviceService implements DeviceService {
 	public void setActuatorValue(int id, String value) throws NoSuchActuatorException {
 		ActuatorResource actuator = actuators.get(id);
 		if (actuator != null) {
-			actuator.toggle();
+			actuator.setValue(value);
 			LOGGER.log(Level.INFO, "Set {0} to value {1}.", new Object[] { actuator.toString(), value });
 		} else {
-			throw new NoSuchActuatorException(String.format("Actuator with ID %d does not exists on the device.", id));
+			throw new NoSuchActuatorException(String.format("Actuator with ID %d does not exist on the device.", id));
 		}
 	}
 
 	public void init() {
 		int counter = 0;
-		// Provision all sensor pins.
 		for (Entry<Integer, SensorResource> entry : sensors.entrySet()) {
 			entry.getValue().init();
 			counter++;
 		}
 		LOGGER.log(Level.INFO, "{0} sensors have been initialised.", new Object[] { counter });
-		// Povision all actuator pins.
 		counter = 0;
 		for (Entry<Integer, ActuatorResource> entry : actuators.entrySet()) {
 			entry.getValue().init();
@@ -151,101 +140,95 @@ public class Pi4jDeviceService implements DeviceService {
 
 	public void destroy() {
 		int counter = 0;
-		// Unprovision all sensor pins.
 		for (Entry<Integer, SensorResource> entry : sensors.entrySet()) {
 			entry.getValue().destroy();
 			counter++;
 		}
 		LOGGER.log(Level.INFO, "{0} sensors have been destroyed.", new Object[] { counter });
 		counter = 0;
-		// Unprovision all actuator pins.
 		for (Entry<Integer, ActuatorResource> entry : actuators.entrySet()) {
 			entry.getValue().destroy();
 			counter++;
 		}
 		LOGGER.log(Level.INFO, "{0} actuators have been destroyed.", new Object[] { counter });
 
-		// Calling GpioController.shutdown() causes
-		// java.util.concurrent.RejectedExecutionException in
-		// listeners after bundle is started again.
-
+		// Calling GpioController.shutdown() causes RejectedExecutionException
+		// in listeners after bundle is started again.
 	}
 
-	private void addSensor(String name, SensorType sensorType, Pin pin) {
-		final int id;
-		if (!sensors.isEmpty()) {
-			id = sensors.lastKey() + 1;
-		} else {
-			id = 0;
-		}
-		switch (sensorType) {
+	private void addSensor(String[] arguments) {
+		SensorResource sensorResource = null;
+		final int id = !sensors.isEmpty() ? sensors.lastKey() + 1 : 0;
+		String name = arguments[0];
+		String type = arguments[1];
+
+		DeviceResourceListener listener = new DeviceResourceListener() {
+
+			@Override
+			public void onChanged(String value) {
+				LOGGER.log(Level.INFO, "Sensor with ID {0} changed value to {1}.", new Object[] { id, value });
+				notifyListenersSensor(id, value);
+			}
+		};
+
+		switch (SensorType.get(type)) {
 		case MERCURY_SWITCH:
-			MercurySwitchResource mercurySwitchResource = new MercurySwitchResource(name, gpioController, pin);
-			mercurySwitchResource.setListener(createSensorGpioPinListener(id));
-			sensors.put(id, mercurySwitchResource);
-			LOGGER.log(Level.INFO, "Added {0}. ID: {1}.", new Object[] { mercurySwitchResource.toString(), id });
+			sensorResource = DeviceResourceFactory.createMercurySwitch(name, listener, gpioController, arguments[2]);
 			break;
 		case TACTILE_SWITCH:
-			TactileSwitchResource tactileSwitchResource = new TactileSwitchResource(name, gpioController, pin);
-			tactileSwitchResource.setListener(createSensorGpioPinListener(id));
-			sensors.put(id, tactileSwitchResource);
-			LOGGER.log(Level.INFO, "Added {0}. ID: {1}.", new Object[] { tactileSwitchResource.toString(), id });
+			sensorResource = DeviceResourceFactory.createTactileSwitch(name, listener, gpioController, arguments[2]);
 			break;
 		case PIR_SENSOR:
-			PirSensorResource pirSensorResource = new PirSensorResource(name, gpioController, pin);
-			pirSensorResource.setListener(createSensorGpioPinListener(id));
-			sensors.put(id, pirSensorResource);
-			LOGGER.log(Level.INFO, "Added {0}. ID: {1}.", new Object[] { pirSensorResource.toString(), id });
+			sensorResource = DeviceResourceFactory.createPirSensor(name, listener, gpioController, arguments[2]);
 			break;
 		default:
 			break;
 		}
+
+		if (sensorResource != null) {
+			sensors.put(id, sensorResource);
+			LOGGER.log(Level.INFO, "Added {0} with ID {1}.", new Object[] { sensorResource.toString(), id });
+		}
 	}
 
-	private void addActuator(String name, ActuatorType actuatorType, Pin pin) {
-		final int id;
-		if (!actuators.isEmpty()) {
-			id = actuators.lastKey() + 1;
-		} else {
-			id = 0;
-		}
-		switch (actuatorType) {
+	private void addActuator(String[] arguments) {
+		ActuatorResource actuatorResource = null;
+		final int id = !actuators.isEmpty() ? actuators.lastKey() + 1 : 0;
+		String name = arguments[0];
+		String type = arguments[1];
+
+		DeviceResourceListener listener = new DeviceResourceListener() {
+
+			@Override
+			public void onChanged(String value) {
+				LOGGER.log(Level.INFO, "Actuator with ID {0} changed value to {1}.", new Object[] { id, value });
+				notifyListenersActuator(id, value);
+			}
+		};
+
+		switch (ActuatorType.get(type)) {
 		case LED:
-			LedResource ledResource = new LedResource(name, gpioController, pin);
-			ledResource.setListener(createActuatorGpioPinListener(id));
-			actuators.put(id, ledResource);
-			LOGGER.log(Level.INFO, "Added {0}. ID: {1}.", new Object[] { ledResource.toString(), id });
+			actuatorResource = DeviceResourceFactory.createLed(name, listener, gpioController, arguments[2]);
 			break;
 		default:
 			break;
 		}
+
+		if (actuatorResource != null) {
+			actuators.put(id, actuatorResource);
+			LOGGER.log(Level.INFO, "Added {0} with ID {1}.", new Object[] { actuatorResource.toString(), id });
+		}
 	}
 
-	private GpioPinListener createSensorGpioPinListener(final int id) {
-		return new GpioPinListenerDigital() {
-
-			@Override
-			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-				for (DeviceListener deviceListener : deviceListeners) {
-					deviceListener.onSensorChanged(id, event.getState().getName());
-				}
-				LOGGER.log(Level.INFO, "Sensor with ID \"{0}\" changed state to \"{1}\".",
-						new Object[] { id, event.getState().getName() });
-			}
-		};
+	private void notifyListenersSensor(int id, String value) {
+		for (DeviceListener deviceListener : deviceListeners) {
+			deviceListener.onSensorChanged(id, value);
+		}
 	}
 
-	private GpioPinListener createActuatorGpioPinListener(final int id) {
-		return new GpioPinListenerDigital() {
-
-			@Override
-			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-				for (DeviceListener deviceListener : deviceListeners) {
-					deviceListener.onActuatorChanged(id, event.getState().getName());
-				}
-				LOGGER.log(Level.INFO, "Actuator with ID \"{0}\" changed state to \"{1}\".",
-						new Object[] { id, event.getState().getName() });
-			}
-		};
+	private void notifyListenersActuator(int id, String value) {
+		for (DeviceListener deviceListener : deviceListeners) {
+			deviceListener.onActuatorChanged(id, value);
+		}
 	}
 }
